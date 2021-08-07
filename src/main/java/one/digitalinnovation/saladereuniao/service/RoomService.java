298d842d446926f246b9a8b206e62c7b7d8308a3
KesiaAmanda <miiizx1@gmail.com>
2021-08-06 23:41:18 -0300

    room service

---
+++ src/main/java/one/digitalinnovation/saladereuniao/service/RoomService.java
@@ -0,0 +1,38 @@
+package one.digitalinnovation.saladereuniao.service;
+
+import lombok.AllArgsConstructor;
+import one.digitalinnovation.saladereuniao.model.Room;
+import one.digitalinnovation.saladereuniao.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class RoomService {
+    private final RoomRepository roomRepository;
+
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
+
+    public Optional<Room> getRoomById(Long id) {
+        return roomRepository.findById(id);
+    }
+
+    public Room createRoom(Room room) {
+        return roomRepository.save(room);
+    }
+
+    public Room updateRoom(Room room) {
+        return roomRepository.save(room);
+    }
+
+    public void deleteRoom(Long id) {
+        roomRepository.deleteById(id);
+    }
+
+
+}

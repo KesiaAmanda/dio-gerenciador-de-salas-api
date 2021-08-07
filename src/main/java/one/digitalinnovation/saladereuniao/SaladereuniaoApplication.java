6ac83a7867bc7da875972dd7ac81beeacde1c32d
KesiaAmanda <miiizx1@gmail.com>
2021-08-06 21:48:01 -0300

    estrutura inicial

---
+++ src/main/java/one/digitalinnovation/saladereuniao/SaladereuniaoApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.saladereuniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaladereuniaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaladereuniaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/saladereuniao/SaladereuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.saladereuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaladereuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

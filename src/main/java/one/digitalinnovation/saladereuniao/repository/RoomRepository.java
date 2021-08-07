a0283ab0ae4deb41a4d1d7271d70ad76ec4e2479
KesiaAmanda <miiizx1@gmail.com>
2021-08-06 22:16:01 -0300

    room repository

---
+++ src/main/java/one/digitalinnovation/saladereuniao/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.saladereuniao.repository;
+
+import one.digitalinnovation.saladereuniao.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+}

943d5b916853a29070d8c6920495bf6c4376e8ea
KesiaAmanda <miiizx1@gmail.com>
2021-08-06 22:36:30 -0300

    exceptions

--- src/main/java/one/digitalinnovation/saladereuniao/exception/ErrorDetails.java
+++ src/main/java/one/digitalinnovation/saladereuniao/exception/ErrorDetails.java
@@ -1,2 +1,13 @@
-package one.digitalinnovation.saladereuniao.exception;public class ErrorDetails {
+package one.digitalinnovation.saladereuniao.exception;
+
+
+import lombok.AllArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
 }
--- src/main/java/one/digitalinnovation/saladereuniao/exception/ResourceNotFoundException.java
+++ src/main/java/one/digitalinnovation/saladereuniao/exception/ResourceNotFoundException.java
@@ -1,2 +1,13 @@
-package one.digitalinnovation.saladereuniao.exception;public class ResourceNotFoundException {
+package one.digitalinnovation.saladereuniao.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception{
+    private static final long serialVersionUID = 1l;
+
+    public ResourceNotFoundException (String message) {
+        super(message);
+    }
 }
